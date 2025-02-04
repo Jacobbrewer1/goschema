f38c3795d997e76c29939e3766ae5f8dcf23e2fd
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-02-04 15:50:03 +0000

    feat(templates): Adding joins capability for the get all methods (#79)
    Adding joins capability for the get all methods

--- magefile.go
+++ magefile.go
@@ -15,7 +15,7 @@ import (
 
 // Default target to run when none is specified
 // If not set, running mage will list available targets
-// var Default = Build
+var Default = Build
 
 // A build step that requires additional params, or platform specific steps for example
 func Build() error {
